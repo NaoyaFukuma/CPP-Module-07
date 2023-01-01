06414b285f96a6a8aaa49333ade6279da0ce3aa7
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-01-02 01:07:30 +0900

    ex02 fin

---
+++ ex02/includes/Array.hpp
@@ -0,0 +1,152 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/31 17:18:18 by nfukuma           #+#    #+#             */
+/*   Updated: 2023/01/02 00:58:24 by nfukuma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template <typename T>
+class Array
+{
+  private:
+	T *arrPtr_;
+	unsigned int size_;
+	void checkIndex(unsigned int i) const;
+
+  public:
+	Array();
+	Array(unsigned int n);
+	Array(const Array &src);
+	~Array();
+
+	unsigned int size() const;
+
+	Array &operator=(const Array &rhs);
+	T &operator[](unsigned int i);
+	const T &operator[](unsigned int i) const;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const Array<T> &arr)
+{
+	unsigned int size = arr.size();
+	for (unsigned int i = 0; i < size; ++i)
+		out << arr[i] << " ";
+	return (out);
+}
+
+template <typename T>
+Array<T>::Array()
+	: arrPtr_(nullptr), size_(0)
+{
+	std::cout << "arrPtr address" << arrPtr_ << std::endl;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+	: size_(n)
+{
+	try
+	{
+		this->arrPtr_ = new T[size_];
+	}
+	catch(const std::bad_alloc& e)
+	{
+		std::cerr << e.what() << '\n';
+		std::exit(EXIT_FAILURE);
+	}
+}
+
+template <typename T>
+Array<T>::Array(const Array &src)
+	: size_(src.size())
+{
+	try
+	{
+		this->arrPtr_ = new T[size_];
+	}
+	catch(const std::bad_alloc& e)
+	{
+		std::cerr << e.what() << '\n';
+		std::exit(EXIT_FAILURE);
+	}
+	for (unsigned int i = 0; i < this->size_; ++i)
+	{
+		this->arrPtr_[i] = src.arrPtr_[i];
+	}
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] arrPtr_;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &rhs)
+{
+	if (this != &rhs)
+	{
+		if (this->size_ != rhs.size())
+		{
+			delete[] this->arrPtr_;
+			this->size_ = rhs.size();
+			try
+			{
+				this->arrPtr_ = new T[this->size_];
+			}
+			catch(const std::bad_alloc& e)
+			{
+				std::cerr << e.what() << '\n';
+				std::exit(EXIT_FAILURE);
+			}
+		}
+		for (unsigned int i = 0; i < size_; ++i)
+			this->arrPtr_[i] = rhs.arrPtr_[i];
+	}
+	return (*this);
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int i)
+{
+	this->checkIndex(i);
+	return (this->arrPtr_[i]);
+}
+
+template <typename T>
+const T &Array<T>::operator[](unsigned int i) const
+{
+	this->checkIndex(i);
+	return (this->arrPtr_[i]);
+}
+
+template <typename T>
+void Array<T>::checkIndex(unsigned int i) const
+{
+	if (i >= this->size())
+	{
+		std::cerr << "Error : Index is out of range.\nThe index you have tried to access. >>> [" << i << "]" << std::endl;
+		std::cerr << "Accessible indexes were 0 to " << this->size() - 1 << "." << std::endl;
+		throw std::exception();
+	}
+}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+	return (this->size_);
+}
+
+
+#endif
---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/31 17:19:07 by nfukuma           #+#    #+#             */
+/*   Updated: 2023/01/02 01:07:09 by nfukuma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+#include "iter.hpp"
+#include <iostream>
+
+#define SIZE 10
+
+
+int	main()
+{
+// Construction with no parameter: Creates an empty array.
+	std::cout << "test 1 : no parameter constractor" << std::endl;
+	{
+		Array<int> test1;
+	}
+
+/* Construction with an unsigned int n as a parameter: Creates an array of n elements
+initialized by default.
+Tip: Try to compile int * a = new int(); then display *a. */
+	{
+		std::cout << "\ntest 2 : take unsined int parameter constractor" << std::endl;
+		Array<int> test2(1);
+		int * a = new int();
+		std::cout << test2 << "vs " << *a << std::endl;
+		delete a;
+	}
+/* Construction by copy and assignment operator. In both cases, modifying either the
+original array or its copy after copying musn’t affect the other array. */
+	std::cout << "\ntest 3 : copy constractor & assignment operator." << std::endl;
+	{
+		Array<int> test3(5);
+		for (int i = 0; i < 5; ++i)
+			test3[i] = i * 2;
+		std::cout << "src array ----> " << test3 << std::endl;
+
+		Array<int> copy3(test3);
+		std::cout << "copy array ---> " << copy3 << std::endl;
+
+		Array<int> assign3(1);
+		assign3 = test3;
+		Array<int> *tmp3 = NULL;
+		tmp3 = &assign3;
+		assign3 = *tmp3;
+		std::cout << "assignment array ---> " << assign3 << std::endl;
+
+	}
+
+	std::cout << "\ntest 4 : [] operator " << std::endl;
+	{
+		try
+		{
+			Array<int> test4(5);
+			for (int i = 0; i < 5; ++i)
+				test4[i] = i * 2;
+			for (int i = 0; i < 6; ++i)
+				std::cout << test4[i] << " " << std::flush;
+			std::cout << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+	}
+	Array<int> test5(5);
+	std::cout << "\ntest 5 : menber function size() \n --> " << test5.size() << std::endl;
+}
