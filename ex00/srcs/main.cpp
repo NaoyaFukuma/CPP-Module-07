927c471efdf5d89598855e5b8b8fd4e37804b574
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-01-07 23:33:21 +0900

    ex00 mod

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 00:08:32 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/12/31 00:09:27 by nfukuma          ###   ########.fr       */
+/*   Updated: 2023/01/07 23:32:53 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,23 @@ int	main(void)
 {
 	int a = 2;
 	int b = 3;
-	::swap(a, b);
+
+	std::cout << "\n\e[32mThis exercise concerns template functions.\e[m\n" << std::endl;
+	std::cout << "for example....\ntemplate <typename T>\nvoid	swap(T &a, T &b);\n\n" << std::endl;
+	std::cout << "test1: type \"int\" " << std::endl;
 	std::cout << "a = " << a << ", b = " << b << std::endl;
+	swap(a, b);
+	std::cout << "swap!!!\na = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
+
+
+	std::cout << "\n\ntest2: type \"string\" " << std::endl;
+	std::string c = "str1";
+	std::string d = "str2";
 	std::cout << "c = " << c << ", d = " << d << std::endl;
+	swap(c, d);
+	std::cout << "swap!!!\nc = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 	return (0);
